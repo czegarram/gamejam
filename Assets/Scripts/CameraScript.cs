50984ae3d998c4a1d213b0d3ed6169e146dfd0a6
Cesar Zegarra <czegarram@gmail.com>
2014-01-25 03:44:40 -0500

    Initial Commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public float pixelsPerUnit;
+	
+	public int zoom = 1;	
+	
+	void Start () { 		
+		Camera.main.orthographicSize = Screen.height / 2f / pixelsPerUnit / zoom;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GroundScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundScript : MonoBehaviour {
+	public float speed = 6.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate (Vector3.right * speed * Time.deltaTime); 
+	}
+}
