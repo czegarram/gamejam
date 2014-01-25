ebe82e9535db3faabc095ce071606e2289796488
Cesar Zegarra <czegarram@gmail.com>
2014-01-25 08:25:28 -0500

    Parallax Background

---
+++ Assets/Scripts/ParallaxScript.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScript : MonoBehaviour {
+	public Transform topScreen;
+	public Transform bottomScreen;
+	public float speed = .6f;
+	public float pixelsToUnit;
+	private float nextpositionBG;
+	private float maxLimitLeftBG;
+
+	private GameObject newTopScreen,newBottomScreen;
+
+	void clonarBGTop ()
+	{
+		newTopScreen = (GameObject)Instantiate (topScreen.gameObject, new Vector3 (topScreen.position.x + nextpositionBG, topScreen.position.y), new Quaternion ());
+	}
+
+	void clonarBGBottom ()
+	{
+		newBottomScreen = (GameObject)Instantiate (bottomScreen.gameObject, new Vector3 (bottomScreen.position.x + nextpositionBG, bottomScreen.position.y), new Quaternion ());
+	}
+
+	void ClonarBackgrounds ()
+	{
+		clonarBGTop ();
+		clonarBGBottom ();
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		nextpositionBG = topScreen.gameObject.GetComponent<SpriteRenderer> ().sprite.texture.width / pixelsToUnit ;
+		maxLimitLeftBG = topScreen.transform.position.x - nextpositionBG;
+		Debug.Log (maxLimitLeftBG);
+		ClonarBackgrounds ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (topScreen.transform.position.x < maxLimitLeftBG) {
+			topScreen.transform.position = new Vector3(newTopScreen.transform.position.x+nextpositionBG,topScreen.transform.position.y);
+			GameObject tmp = newTopScreen;
+			newTopScreen = topScreen.gameObject;
+			topScreen = tmp.transform;
+		} else {
+			topScreen.Translate (Vector3.left * speed * Time.deltaTime); 
+		}
+
+		if (bottomScreen.transform.position.x < maxLimitLeftBG) {
+			bottomScreen.transform.position = new Vector3(newBottomScreen.transform.position.x+nextpositionBG,bottomScreen.transform.position.y);
+			GameObject tmp = newBottomScreen;
+			newBottomScreen = bottomScreen.gameObject;
+			bottomScreen = tmp.transform;
+		} else {
+			bottomScreen.Translate (Vector3.left * speed * Time.deltaTime); 
+		}
+
+		newTopScreen.transform.Translate (Vector3.left * speed * Time.deltaTime); 
+		newBottomScreen.transform.Translate (Vector3.left * speed * Time.deltaTime);
+	}
+}
