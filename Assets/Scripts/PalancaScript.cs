619a38ce55f0a547c1f32be03a3b1a882d3da02d
Cesar Zegarra <czegarram@gmail.com>
2014-01-25 23:40:10 -0500

    Hot Fix 2 LJM

--- Assets/Scripts/PalancaScript.cs
+++ Assets/Scripts/PalancaScript.cs
@@ -4,23 +4,33 @@ using System.Collections;
 public class PalancaScript : MonoBehaviour {
 	public GameObject bloqueo;
 	private Vector2 initialPosition;
+	private bool colision=false;
 	// Use this for initialization
 	void Start () {
 		initialPosition = this.transform.position;
+		bloqueo.rigidbody2D.velocity = new Vector2 (-1f, bloqueo.rigidbody2D.velocity.y);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		bloqueo.rigidbody2D.velocity = new Vector2 (-2f, bloqueo.rigidbody2D.velocity.y);
-		transform.Translate (new Vector3 (-.035f, 0, 0));
+		//bloqueo.rigidbody2D.velocity = new Vector2 (-1f, bloqueo.rigidbody2D.velocity.y);
+		transform.Translate (new Vector3 (-.017f, 0, 0));
 	}
 
 	void OnTriggerEnter2D(Collider2D c){
-		if (Input.GetAxis("Jump") < 0)
-				bloqueo.rigidbody2D.velocity = new Vector2 ( bloqueo.rigidbody2D.velocity.x, 1f);
+		if (c.name.StartsWith ("Tim") && !colision) {
+			if(Input.GetButtonDown ("Jump"))
+				if (Input.GetAxis ("Jump") < 0){
+					bloqueo.rigidbody2D.velocity = new Vector2 (bloqueo.rigidbody2D.velocity.x, 1f);
+					colision=true;
+				}
+		}
 	}
 
 	void OnTriggerExit2D(Collider2D c){
-		bloqueo.rigidbody2D.velocity = new Vector2 ( bloqueo.rigidbody2D.velocity.x, -1f);
+		if (c.name.StartsWith ("Tim") && colision) {
+			bloqueo.rigidbody2D.velocity = new Vector2 (bloqueo.rigidbody2D.velocity.x, -1f);
+			colision=false;
+		}
 	}
 }
