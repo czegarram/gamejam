24449d33fe8865c9d00ddb63be3ab0dac1442d9b
Cesar Zegarra <czegarram@gmail.com>
2014-01-26 09:56:39 -0500

    Mecanica Puerta - Fixed

--- Assets/Scripts/PalancaScript.cs
+++ Assets/Scripts/PalancaScript.cs
@@ -3,10 +3,18 @@ using System.Collections;
 
 public class PalancaScript : MonoBehaviour {
 	public GameObject bloqueo;
+	public bool arriba;
 	private Vector2 initialPosition;
 	private bool colision=false;
+	private int i = 1;
+	private string input;
 	// Use this for initialization
 	void Start () {
+		if (!arriba) {
+			i = -1;
+			input="2";
+
+		}
 		initialPosition = this.transform.position;
 		bloqueo.rigidbody2D.velocity = new Vector2 (-1f, bloqueo.rigidbody2D.velocity.y);
 	}
@@ -14,22 +22,60 @@ public class PalancaScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//bloqueo.rigidbody2D.velocity = new Vector2 (-1f, bloqueo.rigidbody2D.velocity.y);
+		Debug.Log (bloqueo.transform.position);
+
+		if (arriba) {
+			if (bloqueo.transform.position.y > 5) {
+					bloqueo.rigidbody2D.velocity = new Vector2 (bloqueo.rigidbody2D.velocity.x, -1f);
+			}
+			if (bloqueo.transform.position.y < 2.1) {
+					bloqueo.rigidbody2D.velocity = new Vector2 (bloqueo.rigidbody2D.velocity.x, 0f);
+					bloqueo.transform.position = new Vector2 (bloqueo.transform.position.x, 2.2f);
+			}
+
+			if (bloqueo.transform.position.y > 2.2f && bloqueo.transform.position.y < 5f && Input.GetAxis ("Fire") >= 0){
+				bloqueo.rigidbody2D.velocity = new Vector2 (bloqueo.rigidbody2D.velocity.x, -1f*i);
+			}
+		
+		} else {
+			if (bloqueo.transform.position.y < -5) {
+				bloqueo.rigidbody2D.velocity = new Vector2 (bloqueo.rigidbody2D.velocity.x, 1f);
+			}
+			if (bloqueo.transform.position.y > -1.6) {
+				bloqueo.rigidbody2D.velocity = new Vector2 (bloqueo.rigidbody2D.velocity.x, 0f);
+				bloqueo.transform.position = new Vector2 (bloqueo.transform.position.x, -1.7f);
+			}
+			
+			if (bloqueo.transform.position.y > -5 && bloqueo.transform.position.y < -1.8 && Input.GetAxis ("Fire"+input) >= 0){
+				bloqueo.rigidbody2D.velocity = new Vector2 (bloqueo.rigidbody2D.velocity.x, 1f);
+			}
+		}
+
+
 		transform.Translate (new Vector3 (-.017f, 0, 0));
 	}
 
+	void OnTriggerStay2D (Collider2D c){
+	}
+
 	void OnTriggerEnter2D(Collider2D c){
-		if (c.name.StartsWith ("Tim") && !colision) {
-			if(Input.GetButtonDown ("Jump"))
-				if (Input.GetAxis ("Jump") < 0){
-					bloqueo.rigidbody2D.velocity = new Vector2 (bloqueo.rigidbody2D.velocity.x, 1f);
-					colision=true;
-				}
+		if (c.name.StartsWith ("Tim")) {
+			Debug.Log("enter");
+
+			if (Input.GetAxis ("Fire"+input) < 0){
+				bloqueo.rigidbody2D.velocity = new Vector2 (bloqueo.rigidbody2D.velocity.x, 1f*i);					
+				Debug.Log ("velocity");
+			}else{
+		
+			}
+			colision=true;
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D c){
-		if (c.name.StartsWith ("Tim") && colision) {
-			bloqueo.rigidbody2D.velocity = new Vector2 (bloqueo.rigidbody2D.velocity.x, -1f);
+		if (c.name.StartsWith ("Tim") ) {
+			Debug.Log("exit");
+			bloqueo.rigidbody2D.velocity = new Vector2 (bloqueo.rigidbody2D.velocity.x, -1f*i);
 			colision=false;
 		}
 	}
